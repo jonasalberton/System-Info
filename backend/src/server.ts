4df3d0784643c6166c6226f6921c0d5184be40f1
Jonas <jonas.alberton@ellevo.com>
2020-06-24 21:56:25 -0300

    add: build script

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-   return res.json( {
-       hello: 'I am allive'
-   });
+  return res.json({
+    hello: 'I am allive',
+  });
 });
 
 app.listen(3000);
