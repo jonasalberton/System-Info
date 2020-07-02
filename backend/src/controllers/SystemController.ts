6a07447959a69ce3f8fee03163e8c0de92411be4
jonas <jonasceolin@gmail.com>
2020-07-02 13:22:44 -0300

    add system information api

---
+++ backend/src/controllers/SystemController.ts
@@ -0,0 +1,24 @@
+import Express from 'express';
+import SystemInformation from 'systeminformation';
+
+const router = Express.Router();
+
+router.get('/cpu', (request, response) => {
+     SystemInformation.cpu().then( (cpu) => {
+        response.json(cpu);
+    });
+});
+
+router.get('/memory', (request, response) => {
+    SystemInformation.mem().then( (memory) => {
+       response.json(memory);
+   });
+});
+
+router.get('/', (request, response) => {
+    SystemInformation.system().then( (system) => {
+       response.json(system);
+   });
+});
+
+export default router;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
+import SystemController from './controllers/SystemController';
 
 const routes = Router();
 
 routes.use('/user', UserController);
+routes.use('/system', SystemController);
 
 export default routes;
