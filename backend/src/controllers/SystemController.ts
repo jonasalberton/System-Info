89c74e1425e24ada4ca875233b9d4eb2dda7d071
Jonas-Ceolin-Alberton <jonasceolin@gmail.com>
2020-09-17 13:19:03 -0300

    test

--- backend/src/controllers/SystemController.ts
+++ backend/src/controllers/SystemController.ts
@@ -1,30 +1,30 @@
-import Express from 'express';
-import SystemInformation from 'systeminformation';
+import Express from "express";
+import SystemInformation from "systeminformation";
 
 const router = Express.Router();
 
-router.get('/cpu', (request, response) => {
-     SystemInformation.cpu().then( (cpu) => {
-        response.json(cpu);
-    });
+router.get("/cpu", (request, response) => {
+  SystemInformation.cpu().then((cpu) => {
+    response.json(cpu);
+  });
 });
 
-router.get('/memory', (request, response) => {
-    SystemInformation.mem().then( (memory) => {
-       response.json(memory);
-   });
+router.get("/memory", (request, response) => {
+  SystemInformation.mem().then((memory) => {
+    response.json(memory);
+  });
 });
 
-router.get('/', (request, response) => {
-    SystemInformation.system().then( (system) => {
-       response.json(system);
-   });
+router.get("/", (request, response) => {
+  SystemInformation.system().then((system) => {
+    response.json(system);
+  });
 });
 
-router.get('/battery', (request, response) => {
-    SystemInformation.battery().then( (system) => {
-       response.json(system);
-   });
+router.get("/battery", (request, response) => {
+  SystemInformation.battery().then((system) => {
+    response.json(system);
+  });
 });
 
 export default router;
